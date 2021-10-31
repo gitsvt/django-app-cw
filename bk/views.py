be5957ca1753dd09cdfa4f1f701e638d75fbd6c5
gitsvt <89353608+gitsvt@users.noreply.github.com>
2021-10-31 19:55:06 +0300

    Add files

---
+++ bk/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
+
+# Create your views here.
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'bk/signupuser.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            #create new user
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('incandexplist')
+            except IntegrityError:
+                return render(request, 'bk/signupuser.html', {'form':UserCreationForm(), 'error':'Это имя пользователя уже используется!'})
+        else:
+            #сообщить что пароли не совпадают
+            return render(request, 'bk/signupuser.html', {'form':UserCreationForm(), 'error':'Пароли не совпадают!'})
+
+def incandexplist(request):
+    return render(request, 'bk/incandexplist.html')
